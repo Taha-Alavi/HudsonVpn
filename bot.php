5e82cde8533f83c931a80ecf649d9b39627b23b5
Taha Alavi <132651237+Taha-Alavi@users.noreply.github.com>
2023-06-15 00:03:03 +0330

    Update bot.php

--- bot.php
+++ bot.php
@@ -1419,7 +1419,7 @@ $portType = $stmt->get_result()->fetch_assoc()['port_type'];
 $stmt->close();
 
 $rnd = rand(1111,99999);
-$remark = "#{$rnd}-{$srv_remark}-{$from_id}";
+$remark = "{$srv_remark}-{$rnd}-{$from_id}";
 
 if($portType == "auto"){
     file_put_contents('settings/temp.txt',$port.'-'.$last_num);
@@ -2371,7 +2371,7 @@ if(preg_match('/payCustomWithWallet(.*)/',$data, $match)){
     $stmt->close();
 
     $rnd = rand(1111,99999);
-    $remark = "#{$rnd}-{$srv_remark}-{$from_id}";
+    $remark = "{$srv_remark}-{$rnd}-{$from_id}";
     
     if($portType == "auto"){
         file_put_contents('settings/temp.txt',$port.'-'.$last_num);
@@ -2704,7 +2704,7 @@ if(preg_match('/accCustom(.*)/',$data, $match) and $text != $cancelText){
     $stmt->close();
 
     $rnd = rand(1111,99999);
-    $remark = "{$srv_remark}-{$uid}-{$rnd}";
+    $remark = "{$srv_remark}-{$rnd}-{$from_id}";
 
     if($portType == "auto"){
         file_put_contents('settings/temp.txt',$port.'-'.$last_num);
@@ -2941,7 +2941,7 @@ if(preg_match('/payWithWallet(.*)/',$data, $match)){
     $stmt->close();
 
     $rnd = rand(1111,99999);
-    $remark = "#{$rnd}-{$srv_remark}-{$from_id}";
+    $remark = "{$srv_remark}-{$rnd}-{$from_id}";
 
     if($portType == "auto"){
         file_put_contents('settings/temp.txt',$port.'-'.$last_num);
